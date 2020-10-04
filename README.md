628f89ad90f485a2768e913dbf6199451450878d
erinkelsey <erinszilagyi@gmail.com>
2020-10-04 14:13:48 -0600

    Add login and register user functionality

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Secrets App
 
-Whisper app clone with Node.js and Express.
+Whisper app clone with Node.js, Express and MongoDB.
 
 ### Install
 
     $ npm install
+
+### Setup
+
+Create a .env file in the main folder, containing the connection string for your MongoDB. For example, use the following for local connections:
+
+MONGODB_SRV_ADDRESS=mongodb://localhost:27017/blogDB
 
 ### Run
 
--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 /**
  * Setup and initialization.
  */
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -15,7 +18,77 @@ app.use(
 );
 app.use(express.static("public"));
 
-//TODO
+/**
+ * MongoDB and mongoose setup, including schema and models
+ * for User
+ */
+mongoose.connect(process.env.MONGODB_SRV_ADDRESS, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const User = new mongoose.model("User", {
+  email: String,
+  password: String,
+});
+
+/**
+ * GET method for / route.
+ *
+ * Renders the home page.
+ */
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+/**
+ * GET method for /login route.
+ *
+ * Renders the login page.
+ */
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+/**
+ * POST method for /login route.
+ *
+ * Checks if the submitted username/email and password matches a user in the db.
+ * If no match, returns to homepage, else renders the secrets page.
+ */
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.username }, (err, user) => {
+    if (err) console.log(err);
+    else if (user && user.password === req.body.password) res.render("secrets");
+    else res.redirect("/");
+  });
+});
+
+/**
+ * GET method for /register route.
+ *
+ * Renders the register page.
+ */
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+/**
+ * POST method for /register route.
+ *
+ * Registers a user with the submitted username/email and password.
+ * Renders the secrets page once registration has finished.
+ */
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) console.log(err);
+    else res.render("secrets");
+  });
+});
 
 /**
  * Start up server to listen on port 3000.
