d4ae9bb80a6c2c24a27281e58ba829119094e6c6
erinkelsey <erinszilagyi@gmail.com>
2020-10-04 20:25:49 -0600

    Add password securing with hashing and salting with bcrypt

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Whisper app clone with Node.js, Express and MongoDB.
 
-Hashes passwords stored in database with MD5.
+Hashed and salted passwords stored in database with bcrypt. Salt rounds are set to 10, which should take ~10 seconds. Details: https://www.npmjs.com/package/bcrypt
 
 Hosted example:
 
@@ -12,10 +12,20 @@ Hosted example:
 
 ### Setup
 
-Create a .env file in the main folder, containing the connection string for your MongoDB, and a secret key for encrypting the password. For example:
+Create a .env file in the main folder, containing the connection string for your MongoDB. For example:
 
 MONGODB_SRV_ADDRESS=mongodb://localhost:27017/userDB
 
 ### Run
 
     $ npm start
+
+### Different Methods for Securing Passwords in Database:
+
+1. Encrypting and Decrypting passwords with mongoose-encryption
+
+2. Hashing passwords with md5
+
+3. Hashing and salting with bcrypt
+
+NOTE: all of these methods are implemented in this project. Check History to see each of the implementations.
--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 
 /**
  * MongoDB and mongoose setup, including schema and models
- * for User, and encryption setup.
+ * for User.
  */
 mongoose.connect(process.env.MONGODB_SRV_ADDRESS, {
   useNewUrlParser: true,
@@ -33,12 +33,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-// for mongoose-encryption
-// userSchema.plugin(encrypt, {
-//   secret: process.env.SECRET_KEY,
-//   encryptedFields: ["password"],
-// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -69,9 +63,12 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   User.findOne({ email: req.body.username }, (err, user) => {
     if (err) console.log(err);
-    else if (user && user.password === md5(req.body.password))
-      res.render("secrets");
-    else res.redirect("/");
+    else if (user) {
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (result === true) res.render("secrets");
+        else res.redirect("/");
+      });
+    } else res.redirect("/");
   });
 });
 
@@ -91,13 +88,15 @@ app.get("/register", (req, res) => {
  * Renders the secrets page once registration has finished.
  */
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser.save((err) => {
-    if (err) console.log(err);
-    else res.render("secrets");
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save((err) => {
+      if (err) console.log(err);
+      else res.render("secrets");
+    });
   });
 });
 
